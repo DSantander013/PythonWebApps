5446cbb64c2221258445fd2e1d806f8ea4e57c8f
DSantander013 <sant8867@bears.unco.edu>
2024-12-05 00:07:53 -0700

    superhero 3

--- Student/03/Superhero/hero/urls.py
+++ Student/03/Superhero/hero/urls.py
@@ -1,11 +1,11 @@
 from django.urls import path
-from .views import BlackWidow, HulkView, IndexView, IronManView, CaptainAmerica, TheFlash
+from .views import HulkView, IndexView, IronManView, CaptainAmerica, TheFlash, SpiderMan
 
 urlpatterns = [
     path('', IndexView.as_view()),
     path('hulk', HulkView.as_view()),
     path('ironman', IronManView.as_view()),
-    path('blackwidow', BlackWidow.as_view()),
+    path('spiderman', SpiderMan.as_view()),
     path('captainamerica', CaptainAmerica.as_view()),
     path('theflash', TheFlash.as_view()),
 ]
--- Student/03/Superhero/hero/views.py
+++ Student/03/Superhero/hero/views.py
@@ -11,6 +11,13 @@ class HulkView(TemplateView):
     def get_context_data(self, **kwargs):
         return {
             'title': 'Hulk',
+            'id': 'Bruce Banner',
+            'power1': 'super strength',
+            'power2': 'huge',
+            'power3': 'gets tronger when angry',
+            'weakness1': 'radiation',
+            'weakness2': 'not too bright',
+            'weakness3': 'fights with alter ego',
             'body': 'My name is Bruce Banner',
             'image': '/static/images/hulk.jpg'
         }
@@ -22,19 +29,33 @@ class IronManView(TemplateView):
     def get_context_data(self, **kwargs):
         return {
             'title': 'Iron Man',
+            'id': 'Tony Stark',
+            'power1': 'intelligent',
+            'power2': 'nanotech',
+            'power3': 'armor',
+            'weakness1': 'too cocky',
+            'weakness2': 'big ego',
+            'weakness3': 'has something in his chest',
             'body': 'My name is Tony Stark, but I am Iron Man',
             'image': '/static/images/iron_man.jpg'
         }
 
 
-class BlackWidow(TemplateView):
+class SpiderMan(TemplateView):
     template_name = 'hero.html'
 
     def get_context_data(self, **kwargs):
         return {
-            'title': 'Black Widow',
-            'body': 'My name is Natasha Romanova',
-            'image': '/static/images/black_widow.jpg'
+            'title': 'Spider Man',
+            'id': 'Peter Parker',
+            'power1': 'super strength',
+            'power2': 'shoot webs',
+            'power3': 'climb walls',
+            'weakness1': 'a pesticide only works on him',
+            'weakness2': 'family',
+            'weakness3': 'too naive',
+            'body': 'My name is Peter Parker',
+            'image': '/static/images/spiderman1.jpg'
         }
     
 class CaptainAmerica(TemplateView):
@@ -43,6 +64,13 @@ class CaptainAmerica(TemplateView):
     def get_context_data(self, **kwargs):
         return {
             'title': 'Captain America',
+            'id': 'Steve Rogers',
+            'power1': 'enhanced strength',
+            'power2': 'enhanced speed',
+            'power3': 'good with shield',
+            'weakness1': 'human',
+            'weakness2': 'moral code',
+            'weakness3': 'friends',
             'body': 'My name is Steve Rodgers',
             'image': '/static/images/captainamerica.jpg'
         }
@@ -53,6 +81,13 @@ class TheFlash(TemplateView):
     def get_context_data(self, **kwargs):
         return {
             'title': 'The Flash',
+            'id': 'Barry Allen',
+            'power1': 'Super Speed',
+            'power2': 'Super healing',
+            'power3': 'time travel',
+            'weakness1': 'cold',
+            'weakness2': 'love for others',
+            'weakness3': 'family',
             'body': 'My name is Barry Allen',
             'image': '/static/images/theflash.jpg'
         }
