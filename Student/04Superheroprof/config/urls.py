e851106752e88d986795a44759e5a9f109aea367
DSantander013 <sant8867@bears.unco.edu>
2024-12-05 20:10:34 -0700

    proj4

---
+++ Student/04Superheroprof/config/urls.py
@@ -0,0 +1,9 @@
+
+from django.contrib import admin
+from django.urls import path
+from hero.views import HeroDetailView, HeroListView
+
+urlpatterns = [
+    path("", HeroListView.as_view()),
+    path("hero/<str:numID>", HeroDetailView.as_view())
+]
